53cd9894fb3c1616b464d46a4c0690796712baf0
Nikita R <nikita.rabykin@gmail.com>
2026-05-18 00:24:37 +0300

    fix(docs-site): use og-image.png from design for social previews

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/og-image.png',
     colorMode: {
       defaultMode: 'light',
       respectPrefersColorScheme: true,
